08294b7573725e243bf96fc9a602113a57d24ea4
Jasmine Vickery <jasminevickery@Jasmines-Air.config>
2018-07-24 21:16:44 +0100

    Initial Commit Adding enough code to make test to check length of generated word pass

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+/*
+    This is a fantasy language generator written as an excercise that will follow the Object Calisthenics rules
+    https://www.cs.helsinki.fi/u/luontola/tdd-2009/ext/ObjectCalisthenics.pdf and also some of the
+    Functional Calisthenics rules https://codurance.com/2017/10/12/functional-calisthenics/
+    (as much as the language limitations of Kotlin will allow.)
+ */
+
+fun main(args: Array<String>) {
+    val word = WordGenerator.generateWord(WordLength(5))
+    println(word)
+}
+
+data class GeneratedWord(val word: String)
+data class WordLength(val length: Int)
+
+object WordGenerator {
+    fun generateWord(length: WordLength): GeneratedWord {
+        return GeneratedWord("asdfa")
+    }
+}
---
+++ src/test/kotlin/Test.kt
@@ -0,0 +1,12 @@
+import org.junit.Test
+
+class Test {
+    @Test fun wordIsGeneratedToGivenLength() {
+        // Given a word length of 5
+        val wordLength = WordLength(5)
+        // When a word is generated
+        val generatedWord = WordGenerator.generateWord(wordLength)
+        // Then it has a length equal to 5
+        assert(generatedWord.word.length == 5)
+    }
+}
